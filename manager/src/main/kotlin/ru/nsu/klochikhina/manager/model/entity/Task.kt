e456eb36962a82404c812760935e1a5cca2efeac
sklochikhina <klochikhina.sofya@mail.ru>
2026-03-17 00:27:50 +0700

    Добавлена сущность для тасок

---
+++ manager/src/main/kotlin/ru/nsu/klochikhina/manager/model/entity/Task.kt
@@ -0,0 +1,25 @@
+package ru.nsu.klochikhina.manager.model.entity
+
+import enums.TaskStatus
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
+
+@Document(collection = "tasks")
+@CompoundIndex(name = "req_start_idx", def = "{'requestId': 1, 'startIndex': 1}", unique = true)
+data class Task (
+    @Id val id: String? = null,
+    val requestId: String,
+    val startIndex: Long,
+    val count: Long,
+    val targetHash: String,
+    val maxLength: Int,
+    val status: TaskStatus = TaskStatus.QUEUED,
+
+    val owner: String? = null,
+    val ownerLockUntil: Instant? = null,
+
+    val createdAt: Instant = Instant.now(),
+    val updatedAt: Instant = Instant.now()
+)
